31511dd36538db305c9db64f0d9c8393f0188d19
taylor <taylor@baremetalcloud.com>
2020-04-27 11:11:07 -0400

    Converted search box into a component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import {CardList} from "./components/card-list/card-list.component";
-
+import {SearchBox} from "./components/search-box/search-box.component";
 import './App.css';
 
 class App extends Component {
@@ -29,10 +29,7 @@ class App extends Component {
         )
         return (
             <div className="App">
-                <input
-                    type="search"
-                    placeholder='search monsters'
-                    onChange={event => this.setState({ searchField: event.target.value})}/>
+                <SearchBox placeholder='search monsters' handleChange={event => this.setState({ searchField: event.target.value})} />
                 <CardList monsters={filteredMonsters} />
             </div>
         )
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { Card } from "../card/card.component";
 
-import './card-list.style.css'
+import './card-list.styles.css'
 
 export const CardList = (props) => {
     return (
