479eb9f7b4d3c85a80cb28f3caf582d8ffc6586b
taylor <taylor@baremetalcloud.com>
2020-04-27 12:43:07 -0400

    Updates

---
+++ precache-manifest.900cadb822c20b2d716d571ee8b37b2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596191d448a53f09f1b6721c7a5aaecf",
+    "url": "/taylor009/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "dba0913fdf4a4da21ada",
+    "url": "/taylor009/Monster-Rolodex/static/css/main.2f739385.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/taylor009/Monster-Rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/taylor009/Monster-Rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dba0913fdf4a4da21ada",
+    "url": "/taylor009/Monster-Rolodex/static/js/main.f97b9445.chunk.js"
+  },
+  {
+    "revision": "041b989b7e9a4f2d963d",
+    "url": "/taylor009/Monster-Rolodex/static/js/runtime-main.08359b05.js"
+  }
+]);
